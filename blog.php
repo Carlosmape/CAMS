a8f37fe95a838fe512a503aec447e6a8984c08f8
Carlos Martín <cagrmape@gmail.com>
2019-02-19 18:58:37 +0000

    Adding error pages

--- blog.php
+++ blog.php
@@ -1,11 +1,17 @@
 <?php
-	require "blog/header.php";
-	if (isset($_GET['post'])){
-		require "blog/post.php";
+	if (file_exists("install/index.php")){
+		echo "Aun no ha terminado la instalación. Ejecute suip/INSTALL en su navegador";
 	}else{
-		require "blog/blog.php";
+		require "blog/header.php";
+		
+		if (isset($_GET['post'])){
+			require "blog/post.php";
+		}else{
+			require "blog/blog.php";
+		}
+		
+		require "blog/sidebar.php";
+		require "blog/footer.php";
 	}
-	
-	require "blog/sidebar.php";
-	require "blog/footer.php";
+
 ?>
