5dd26139c7e87c7d78d4cf3e45d77396b716581b
Carlos Martín <cagrmape@gmail.com>
2019-04-17 19:29:58 +0100

    Update setup.php

--- install/setup.php
+++ install/setup.php
@@ -118,7 +118,7 @@ if (isset($_POST['setup'])){
 					`LINE` INT NULL , 
 					`PROCCESS` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL , 
 					`SESSION_VALUE` TEXT CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL ,
-					`DATE` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP);")){
+					`DATE` DATE NOT NULL);")){
 					echo "Tables created.";
 					if (mysqli_query($connection,
 						"INSERT INTO USERS (`USER`,`MAIL`,`PASSWORD`, `TYPE`)
