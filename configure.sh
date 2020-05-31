24fa8eaff1aafdedec285d2da95e31cec09f9e5e
CarlosMaPe <cagrmape@gmail.com>
2020-05-31 17:41:03 +0200

    Improving installation/deployment scripts, fixing some dependencies during installing from scratch

--- configure.sh
+++ configure.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-sudo apt-get install mysql-server*
-sudo apt-get install lighttpd
-#adding php support
+
+# Install some dependencies
+sudo apt install mysql-server
+sudo apt install lighttpd
+sudo apt install php
+sudo apt install php-cgi
+# Adding php support
 sudo lighty-enable-mod fastcgi 
 sudo lighty-enable-mod fastcgi-php
-clear
-if(sudo cp -r * /var/www/html/)then
-	echo "#CAMS copied to /var/www/html/"
+sudo chmod 755 /var/log/lighttpd
+sudo chown www-data:www-data /var/log/lighttpd
+# Clear upper console output. FIXME: Is it really good idea??
+# clear
+
+# Copy needed files on correspondant sytem folder
+if(sudo ./deploy.sh) then
+  echo "#CAMS has been configured!. Go to http://yoursite.com/install to do final step"
 fi
-if(sudo service lighttpd force-reload)then
-	sudo chown -R www-data /var/www/html/*
-	echo "#CAMS has been configured!. Go to http://yoursite.com/install to do final step"
-fi
+
+# Remove configure file on production folder
 sudo rm /var/www/html/configure.sh
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+##############################################################
+# This file shall copy current folder files to /var/www/html
+# Used during developing and production time. Needs super user
+# priviledges to work properly
+##############################################################
+
+#!/bin/bash
+
+if(sudo cp -r * /var/www/html/)then
+	echo "#CAMS copied to /var/www/html/"
+fi
+if(sudo service lighttpd stop && sudo service lighttpd start) then
+	sudo chown -R www-data /var/www/html/*
+fi
+
