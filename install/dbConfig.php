64f92033f6709ae86b33ac6f5c7a5b0a5c138b87
Carlosmape <cagrmape@gmail.com>
2021-09-20 17:44:05 +0200

    Improvings over DDBB architechture and installation .SHOULD review installation process

--- install/dbConfig.php
+++ install/dbConfig.php
@@ -8,11 +8,7 @@ function CreateDatabase($connection){
 function CreateTablesStructures($connection){
 	try{
 		$creationResult = true;
-		$creationResult &= CreateUsersTable($connection);
-		$creationResult &= CreateRecordTable($connection);
-		$creationResult	&= CreateCategoriesTable($connection);
-		$creationResult	&= CreateArticlesTable($connection);
-		$creationResult	&= CreateLogTable($connection);	
+		$creationResult &= CreateTables($connection);
 		echo "Tables created.";
 	} catch (Exception $e){
 		echo'"Error CAMS: Could not create necesary tables"' + $e->getMessage();
@@ -21,73 +17,141 @@ function CreateTablesStructures($connection){
 	return $creationResult;
 }
 
-function CreateUsersTable($connection){
+function CreateTables($connection){
 	try {
 		return mysqli_query($connection,
-		"CREATE TABLE IF NOT EXISTS USERS(
-		`ID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		`USER` VARCHAR(32),
-		`MAIL` VARCHAR(32),
-		`PASSWORD` VARCHAR(64),
-		`TYPE` INT(11),
-		UNIQUE (`USER`,`MAIL`),
-		CHECK (TYPE BETWEEN 0 AND 1));"); 
+"-- Adminer 4.8.1 MySQL 5.5.5-10.3.29-MariaDB-0+deb10u1 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `ARTICLES`;
+CREATE TABLE `ARTICLES` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TITLE` varchar(96) DEFAULT NULL,
+  `IMAGEHEADER` text DEFAULT NULL,
+  `TYPE` int(11) DEFAULT NULL,
+  `CATEGORY` int(11) DEFAULT NULL,
+  `CONTENT` text DEFAULT NULL,
+  `PINNED` bit(1) DEFAULT NULL,
+  `AUTOR` int(11) DEFAULT NULL,
+  `DATE` date DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `TITLE` (`TITLE`),
+  KEY `AUTOR` (`AUTOR`),
+  CONSTRAINT `ARTICLES_ibfk_1` FOREIGN KEY (`AUTOR`) REFERENCES `USERS` (`ID`),
+  CONSTRAINT `CONSTRAINT_1` CHECK (`TYPE` between 0 and 2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `CATEGORIES`;
+CREATE TABLE `CATEGORIES` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PARENTID` int(11) DEFAULT NULL,
+  `TITLE` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `TITLE` (`TITLE`),
+  KEY `PARENTID` (`PARENTID`),
+  CONSTRAINT `CATEGORIES_ibfk_1` FOREIGN KEY (`PARENTID`) REFERENCES `CATEGORIES` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `LOG`;
+CREATE TABLE `LOG` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `LEVEL` int(11) NOT NULL,
+  `MESSAGE` text NOT NULL,
+  `FILE` varchar(255) DEFAULT NULL,
+  `LINE` int(11) DEFAULT NULL,
+  `PROCCESS` varchar(255) NOT NULL,
+  `SESSION_VALUE` text DEFAULT NULL,
+  `DATE` datetime NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `PERMISSIONS`;
+CREATE TABLE `PERMISSIONS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CREATE` bit(1) NOT NULL,
+  `EDIT` bit(1) NOT NULL,
+  `DELETE` bit(1) NOT NULL,
+  `ENTITY` varchar(96) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `PERMISSIONS` (`ID`, `CREATE`, `EDIT`, `DELETE`, `ENTITY`) VALUES
+(1,	CONV('1', 2, 10) + 0,	CONV('1', 2, 10) + 0,	CONV('1', 2, 10) + 0,	'ALL'),
+(2,	CONV('1', 2, 10) + 0,	CONV('1', 2, 10) + 0,	CONV('1', 2, 10) + 0,	'ARTICLES');
+
+DROP TABLE IF EXISTS `RECORD`;
+CREATE TABLE `RECORD` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ACTION` varchar(45) DEFAULT NULL,
+  `AUTOR` int(11) NOT NULL,
+  `RECIBER` int(11) NOT NULL,
+  `RECIBERCONTEXT` varchar(45) NOT NULL,
+  `DATE` datetime NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `AUTOR` (`AUTOR`),
+  CONSTRAINT `RECORD_ibfk_1` FOREIGN KEY (`AUTOR`) REFERENCES `USERS` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `ROLES`;
+CREATE TABLE `ROLES` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(96) NOT NULL,
+  `DESCRIPTION` varchar(96) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `ROLES` (`ID`, `NAME`, `DESCRIPTION`) VALUES
+(0,	'Admin',	'Website administrator'),
+(1,	'Contributor',	'Website contributor (allowed to upload content)');
+
+DROP TABLE IF EXISTS `ROLES_PERMISSIONS`;
+CREATE TABLE `ROLES_PERMISSIONS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_ROLE` int(11) NOT NULL,
+  `ID_PERMISSION` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_ROLE` (`ID_ROLE`),
+  KEY `ID_PERMISSION` (`ID_PERMISSION`),
+  CONSTRAINT `ROLES_PERMISSIONS_ibfk_1` FOREIGN KEY (`ID_ROLE`) REFERENCES `ROLES` (`ID`),
+  CONSTRAINT `ROLES_PERMISSIONS_ibfk_2` FOREIGN KEY (`ID_PERMISSION`) REFERENCES `PERMISSIONS` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `USERS`;
+CREATE TABLE `USERS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `USER` varchar(32) NOT NULL,
+  `MAIL` varchar(32) NOT NULL,
+  `PASSWORD` varchar(64) NOT NULL,
+  `TYPE` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `USER` (`USER`,`MAIL`),
+  KEY `TYPE` (`TYPE`),
+  CONSTRAINT `USERS_ibfk_1` FOREIGN KEY (`TYPE`) REFERENCES `ROLES` (`ID`),
+  CONSTRAINT `CONSTRAINT_1` CHECK (`TYPE` between 0 and 1)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `USERS` (`ID`, `USER`, `MAIL`, `PASSWORD`, `TYPE`) VALUES
+(1,	'admin',	'example@mail.com',	'e3afed0047b08059d0fada10f400c1e5',	0),
+(6,	'user',	'sample@mymail.com',	'ee11cbb19052e40b07aac0ca060c23ee',	1);
+
+INSERT INTO `ARTICLES` (`ID`, `TITLE`, `IMAGEHEADER`, `TYPE`, `CATEGORY`, `CONTENT`, `PINNED`, `AUTOR`, `DATE`) VALUES
+(1,	'Lorem ipsum dolor',	'http://www.highreshdwallpapers.com/wp-content/uploads/2012/10/Lorem-Ipsum-Wallpaper.jpg',	1,	0,	'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.',	NULL,	NULL,	'2021-09-19');
+
+-- 2021-09-20 15:36:32"); 
 	} catch (Exception $e){
 		throw $e;
 	}
 }
 
-function CreateRecordTable($connection){
-	try{
-		return mysqli_query($connection,
-		"CREATE TABLE IF NOT EXISTS `RECORD` (
-		`ID` INT(11) NOT NULL AUTO_INCREMENT,
-		`ACTION` VARCHAR(45) NULL,
-		`AUTOR` INT(11) NOT NULL,
-		`RECIBER` INT(11) NOT NULL,
-		`RECIBERCONTEXT` VARCHAR(45) NOT NULL,
-		`DATE` DATETIME NOT NULL,
-		PRIMARY KEY (`ID`));"); 
-	} catch (Exception $e) {
-		throw $e;
-	}
-}
-
-function CreateCategoriesTable($connection){
-	try{
-		return mysqli_query($connection,
-		"CREATE TABLE IF NOT EXISTS `CATEGORIES`(
-		`ID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		`PARENTID` INT(11),
-		`TITLE` VARCHAR(32) UNIQUE;"); 
-	} catch(Exception $e){
-		throw $e;
-	}
-}
-function CreateArticlesTable($connection){
-	mysqli_query($connection,
-	"CREATE TABLE IF NOT EXISTS `ARTICLES`(
-	`ID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`TITLE` VARCHAR(96) UNIQUE,
-	`TYPE` INT(11),
-	`CATEGORIES` INT(11),
-	`DATE` DATE,
-	`CONTENT`	TEXT,
-	`IMAGEHEADER` TEXT,
-	`AUTOR` INT(11),
-	CHECK (TYPE BETWEEN 0 AND 2));");
-
-}
-function CreateLogTable($connection){
-	mysqli_query($connection, 
-	"CREATE TABLE IF NOT EXISTS `LOG` ( 
-	`ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-	`LEVEL` INT NOT NULL , 
-	`MESSAGE` TEXT NOT NULL , 
-	`FILE` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL , 
-	`LINE` INT NULL , 
-	`PROCCESS` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL , 
-	`SESSION_VALUE` TEXT CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL ,
-	`DATE` DATE NOT NULL);");
-}
 ?>
