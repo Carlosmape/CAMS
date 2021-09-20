3a4f33ae7b6ddefdb2e29a2a97fc4d470c2f6726
Carlosmape <cagrmape@gmail.com>
2021-09-20 09:22:22 +0200

    trying to properly retrieve categories count

--- cams/modules/dashboard.php
+++ cams/modules/dashboard.php
@@ -70,7 +70,7 @@ if (isset($_SESSION['connection']) && !$_SESSION['connection']->timeout()) { //y
 									</div>
 									<div class="col-xs-6 col-sm-4 col-md-2 placeholder text-center">
 										<i class="material-icons" style="font-size: 48px;">category</i>
-										<h4>Categories - <span class="text-muted"><?php echo mysqli_fetch_array($database->countCategories())['COUNT(*)']?></span></h4>
+										<h4>Categories - <span class="text-muted"><?php echo mysqli_fetch_array($database->countCategories())[0]?></span></h4>
 									</div>
 									<div class="col-xs-6 col-sm-4 col-md-2 placeholder text-center">
 									<i class="material-icons" style="font-size: 48px;">save</i>
