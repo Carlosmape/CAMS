3d9f6bcf3427801a4ab52dd5fd4d6e862b487c1e
Carlosmape <cagrmape@gmail.com>
2021-09-21 16:55:49 +0200

    solved possible error

--- cams/modules/roles/createRole.php
+++ cams/modules/roles/createRole.php
@@ -1,26 +1,26 @@
 <?php
-	require "../../includes/class/connection.php";
-	require "../../includes/config.php";
-	require "../../includes/sqlfunctions.php";
-	if (isset($_SESSION['connection']) && !$_SESSION['connection']->timeout() && $_SESSION['connection']->isAdmin()) { //if you are connected
-		$_SESSION['connection']->keepalive(); //refresh connection timeout
-		$database = new Sqlconnection;//connect to database in order to extract roles info
-		if (isset($database)){
-			if($database->addRole(strip_tags($_POST['Name']), strip_tags($_POST['Description']))){
-				echo "Role added!";
-				$role = $database->getRole($_POST['Name']);
-				var_dump($role);
-				//Assign given permissions
-				foreach($_POST['Permission'] as $permission){
-					$database->addPermissionToRole($role['ID'], $permission);
-				}
+require "../../includes/class/connection.php";
+require "../../includes/config.php";
+require "../../includes/sqlfunctions.php";
+if (isset($_SESSION['connection']) && !$_SESSION['connection']->timeout() && $_SESSION['connection']->isAdmin()) { //if you are connected
+	$_SESSION['connection']->keepalive(); //refresh connection timeout
+	$database = new Sqlconnection;//connect to database in order to extract roles info
+	if (isset($database)){
+		if($database->addRole(strip_tags($_POST['Name']), strip_tags($_POST['Description']))){
+			echo "Role added!";
+			$role = $database->getRole(strip_tags($_POST['Name']));
+			var_dump($role);
+			//Assign given permissions
+			foreach($_POST['Permission'] as $permission){
+				$database->addPermissionToRole($role['ID'], $permission);
 			}
-			else{
-				echo "Cant create role";
-			}
-		}else{
-		  echo "Error CAMS could not connect to your DATABASE";
 		}
-		
+		else{
+			echo "Cant create role";
+		}
+	}else{
+		echo "Error CAMS could not connect to your DATABASE";
 	}
- ?>
+
+}
+?>
