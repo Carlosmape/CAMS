f0b75831835c75b58e7dae0c8540ceb0b2891448
Carlosmape <cagrmape@gmail.com>
2021-09-21 15:20:13 +0200

    Add var_dump to know how is giving permissions multi selection field

--- cams/modules/roles/createRole.php
+++ cams/modules/roles/createRole.php
@@ -6,12 +6,13 @@
 		$_SESSION['connection']->keepalive(); //refresh connection timeout
 		$database = new Sqlconnection;//connect to database in order to extract roles info
 		if (isset($database)){
-			if($database->addRole(strip_tags($_POST['Name']), strip_tags($_POST['Description']))){
+			var_dump($_POST['Permissions']);
+			/*if($database->addRole(strip_tags($_POST['Name']), strip_tags($_POST['Description']))){
 				echo "Role added!";
 			}
 			else{
 				echo "Cant create role";
-			}
+			}*/
 		}else{
 		  echo "Error CAMS could not connect to your DATABASE";
 		}
