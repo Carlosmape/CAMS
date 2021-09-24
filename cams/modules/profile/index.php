bc1bb7cf630012b2564c27a1262e4fe1465b1b0c
Carlosmape <cagrmape@gmail.com>
2021-09-24 10:10:09 +0200

    Improved formulary visualization

--- cams/modules/profile/index.php
+++ cams/modules/profile/index.php
@@ -12,20 +12,21 @@
 				<div class="col-sm-9 col-md-10 main">
 					<form id="editForm" class="form-horizontal">
 						<div class="form-group">
-							<input class="col-sm-8" type="text" id="editID" name="editID" placeholder="" hidden required value="<?php echo $user['ID'];?>">
-							<input class="col-sm-8" type="text" id="editType" name="editType" placeholder="" hidden required value="<?php echo $user['TYPE'];?>">
+							<input type="text" id="editID" name="editID" placeholder="" hidden required value="<?php echo $user['ID'];?>">
+							<input type="text" id="editType" name="editType" placeholder="" hidden required value="<?php echo $user['TYPE'];?>">
 						</div>
 						<div class="form-group">
-							<label class="control-label col-sm-2" for="editUser">User:</label>
-							<input class="form-control col-sm-8" type="text" id="editUser" name="editUser" placeholder="" required value="<?php echo $user['USER'];?>">
+							<label class="control-label" for="editUser">User:</label>
+							<input class="form-control" type="text" id="editUser" name="editUser" placeholder="" required value="<?php echo $user['USER'];?>">
 						</div>
 						<div class="form-group">
-							<label class="control-label col-sm-2" for="editMail">Mail:</label>
-							<input class="form-control col-sm-8" type="text" id="editMail" name="editMail" placeholder="" value="<?php echo $user['MAIL'];?>">
+							<label class="control-label" for="editMail">Mail:</label>
+							<input class="form-control" type="text" id="editMail" name="editMail" placeholder="" value="<?php echo $user['MAIL'];?>">
 						</div>
 						<div class="form-group">
-							<label class="control-label col-sm-2" for="email">New password:</label>
-							<input class="form-control col-sm-8" type="text" id="editPass" name="editPass" placeholder="New pass...">
+							<label class="control-label" for="email">New password:</label>
+							<span class="badge badge-info">Let empty to do not change it</span>
+							<input class="form-control" type="text" id="editPass" name="editPass" placeholder="New pass...">
 						</div>						
 						<div class="form-group">
 							<input class="btn btn-info" type="button" id="EditProfile" name="EditProfile" value="Edit">
