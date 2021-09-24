9c5e50a5a8caf984b027edd902576f9d89a7257d
Carlosmape <cagrmape@gmail.com>
2021-09-24 18:01:48 +0200

    Added latest changes in the DDBB

--- install/dbConfig.php
+++ install/dbConfig.php
@@ -21,6 +21,8 @@ function CreateTables($connection){
 	try {
 		return mysqli_query($connection,"
 
+-- Adminer 4.8.1 MySQL 5.5.5-10.3.29-MariaDB-0+deb10u1 dump
+
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -33,7 +35,6 @@ CREATE TABLE `ARTICLES` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `TITLE` varchar(96) DEFAULT NULL,
   `IMAGEHEADER` text DEFAULT NULL,
-  --`TYPE` int(11) DEFAULT NULL,
   `CATEGORY` int(11) DEFAULT NULL,
   `CONTENT` text DEFAULT NULL,
   `PINNED` bit(1) DEFAULT NULL,
@@ -42,9 +43,46 @@ CREATE TABLE `ARTICLES` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `TITLE` (`TITLE`),
   KEY `AUTOR` (`AUTOR`),
-  CONSTRAINT `ARTICLES_ibfk_1` FOREIGN KEY (`AUTOR`) REFERENCES `USERS` (`ID`),
-  CONSTRAINT `CONSTRAINT_1` CHECK (`TYPE` between 0 and 2)
+  CONSTRAINT `ARTICLES_ibfk_1` FOREIGN KEY (`AUTOR`) REFERENCES `USERS` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `ARTICLES` (`ID`, `TITLE`, `IMAGEHEADER`, `CATEGORY`, `CONTENT`, `PINNED`, `AUTOR`, `DATE`) VALUES
+(1,	'Lorem ipsum dolor',	'http://www.highreshdwallpapers.com/wp-content/uploads/2012/10/Lorem-Ipsum-Wallpaper.jpg',	NULL,	'<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. y tal</p>',	NULL,	NULL,	'2021-09-19');
+
+DROP TABLE IF EXISTS `CATEGORIES`;
+CREATE TABLE `CATEGORIES` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PARENTID` int(11) DEFAULT NULL,
+  `TITLE` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `TITLE` (`TITLE`),
+  KEY `PARENTID` (`PARENTID`),
+  CONSTRAINT `CATEGORIES_ibfk_1` FOREIGN KEY (`PARENTID`) REFERENCES `CATEGORIES` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `CATEGORIES` (`ID`, `PARENTID`, `TITLE`) VALUES
+(1,	NULL,	'A cat'),
+(2,	1,	'A child category'),
+(10,	NULL,	'Another parent category');
+
+DROP TABLE IF EXISTS `LOG`;
+CREATE TABLE `LOG` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `LEVEL` int(11) NOT NULL,
+  `MESSAGE` text NOT NULL,
+  `FILE` varchar(255) DEFAULT NULL,
+  `LINE` int(11) DEFAULT NULL,
+  `PROCCESS` varchar(255) NOT NULL,
+  `SESSION_VALUE` text DEFAULT NULL,
+  `DATE` datetime NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `LOG` (`ID`, `LEVEL`, `MESSAGE`, `FILE`, `LINE`, `PROCCESS`, `SESSION_VALUE`, `DATE`) VALUES
+(6,	3,	'',	'/var/www/html/cams/modules/record.php',	13,	'',	'a:1:{s:10:\"connection\";O:10:\"Connection\":4:{s:4:\"time\";i:1632388205;s:4:\"user\";s:5:\"admin\";s:4:\"type\";s:1:\"0\";s:6:\"userid\";s:1:\"1\";}}',	'2021-09-23 10:10:05'),
+(7,	3,	'',	'/var/www/html/cams/modules/record.php',	13,	'',	'a:1:{s:10:\"connection\";O:10:\"Connection\":4:{s:4:\"time\";i:1632388558;s:4:\"user\";s:5:\"admin\";s:4:\"type\";s:1:\"0\";s:6:\"userid\";s:1:\"1\";}}',	'2021-09-23 10:15:58'),
+(8,	3,	'',	'/var/www/html/cams/index.php',	8,	'#0 {main}',	'a:1:{s:10:\"connection\";O:10:\"Connection\":4:{s:4:\"time\";i:1632470015;s:4:\"user\";s:5:\"admin\";s:4:\"type\";s:1:\"0\";s:6:\"userid\";s:1:\"1\";}}',	'2021-09-24 09:01:15'),
+(9,	3,	'',	'/var/www/html/cams/index.php',	8,	'#0 {main}',	'a:1:{s:10:\"connection\";O:10:\"Connection\":4:{s:4:\"time\";i:1632470015;s:4:\"user\";s:5:\"admin\";s:4:\"type\";s:1:\"0\";s:6:\"userid\";s:1:\"1\";}}',	'2021-09-24 09:01:33');
 
 DROP TABLE IF EXISTS `PAGES`;
 CREATE TABLE `PAGES` (
@@ -63,30 +101,9 @@ CREATE TABLE `PAGES` (
   CONSTRAINT `CONSTRAINT_1` CHECK (`TYPE` between 0 and 2)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `CATEGORIES`;
-CREATE TABLE `CATEGORIES` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PARENTID` int(11) DEFAULT NULL,
-  `TITLE` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `TITLE` (`TITLE`),
-  KEY `PARENTID` (`PARENTID`),
-  CONSTRAINT `CATEGORIES_ibfk_1` FOREIGN KEY (`PARENTID`) REFERENCES `CATEGORIES` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-DROP TABLE IF EXISTS `LOG`;
-CREATE TABLE `LOG` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `LEVEL` int(11) NOT NULL,
-  `MESSAGE` text NOT NULL,
-  `FILE` varchar(255) DEFAULT NULL,
-  `LINE` int(11) DEFAULT NULL,
-  `PROCCESS` varchar(255) NOT NULL,
-  `SESSION_VALUE` text DEFAULT NULL,
-  `DATE` datetime NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `PAGES` (`ID`, `TITLE`, `IMAGEHEADER`, `TYPE`, `CATEGORY`, `CONTENT`, `PINNED`, `AUTOR`, `DATE`) VALUES
+(1,	'asdasd',	'',	0,	NULL,	'<p>asdasdasdlsfdmksldmflasmdflsfdm-alsdvmlvfmldflkvmsldcms</p>',	NULL,	1,	'2021-09-22'),
+(2,	'A page y tal',	'',	0,	1,	'<p>A content</p>',	NULL,	1,	'2021-09-22');
 
 DROP TABLE IF EXISTS `RECORD`;
 CREATE TABLE `RECORD` (
@@ -101,6 +118,12 @@ CREATE TABLE `RECORD` (
   CONSTRAINT `RECORD_ibfk_1` FOREIGN KEY (`AUTOR`) REFERENCES `USERS` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+INSERT INTO `RECORD` (`ID`, `ACTION`, `AUTOR`, `RECIBER`, `RECIBERCONTEXT`, `DATE`) VALUES
+(1,	'SAVE',	1,	0,	'SETTINGS',	'2021-09-20 07:56:59'),
+(2,	'EDIT',	1,	2,	'ARTICLES',	'2021-09-22 16:32:28'),
+(3,	'EDIT',	1,	1,	'ARTICLES',	'2021-09-24 15:10:35'),
+(4,	'EDIT',	1,	1,	'ARTICLES',	'2021-09-24 15:10:40'),
+(5,	'EDIT',	1,	7,	'ARTICLES',	'2021-09-24 15:11:09');
 
 DROP TABLE IF EXISTS `ROLES`;
 CREATE TABLE `ROLES` (
@@ -113,20 +136,6 @@ CREATE TABLE `ROLES` (
 INSERT INTO `ROLES` (`ID`, `NAME`, `DESCRIPTION`) VALUES
 (0,	'Admin',	'Website administrator'),
 (1,	'Contributor',	'Website contributor (allowed to upload content)');
-
-DROP TABLE IF EXISTS `SECTIONS`;
-CREATE TABLE `SECTIONS` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ENTITY` varchar(96) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `SECTIONS` (`ID`, `ENTITY`) VALUES
-(0,	'ALL'),
-(1,	'ARTICLES'),
-(2,	'CATEGORIES'),
-(3,	'USERS');
-
 
 DROP TABLE IF EXISTS `ROLES_PERMISSIONS`;
 CREATE TABLE `ROLES_PERMISSIONS` (
@@ -141,8 +150,21 @@ CREATE TABLE `ROLES_PERMISSIONS` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `ROLES_PERMISSIONS` (`ID`, `ID_ROLE`, `ID_PERMISSION`) VALUES
-(0,	0,	0),
-(1,	1,	1);
+(1,	0,	0),
+(5,	1,	1);
+
+DROP TABLE IF EXISTS `SECTIONS`;
+CREATE TABLE `SECTIONS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ENTITY` varchar(96) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `SECTIONS` (`ID`, `ENTITY`) VALUES
+(0,	'ALL'),
+(1,	'ARTICLES'),
+(2,	'CATEGORIES'),
+(3,	'USERS');
 
 DROP TABLE IF EXISTS `USERS`;
 CREATE TABLE `USERS` (
@@ -159,13 +181,10 @@ CREATE TABLE `USERS` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `USERS` (`ID`, `USER`, `MAIL`, `PASSWORD`, `TYPE`) VALUES
-(1,	'admin',	'example@mail.com',	'e3afed0047b08059d0fada10f400c1e5',	0),
-(6,	'user',	'sample@mymail.com',	'ee11cbb19052e40b07aac0ca060c23ee',	1);
+(1,	'Admin',	'admin@mail.com',	'e3afed0047b08059d0fada10f400c1e5',	0),
+(26,	'sample',	'sample@sample.com',	'5e8ff9bf55ba3508199d22e984129be6',	1);
 
-INSERT INTO `ARTICLES` (`ID`, `TITLE`, `IMAGEHEADER`, `CATEGORY`, `CONTENT`, `PINNED`, `AUTOR`, `DATE`) VALUES
-(1,	'Lorem ipsum dolor',	'http://www.highreshdwallpapers.com/wp-content/uploads/2012/10/Lorem-Ipsum-Wallpaper.jpg',	NULL,	'<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. y tal</p>',	NULL,	NULL,	'2021-09-19');
-
-"); 
+-- 2021-09-24 15:59:29"); 
 	} catch (Exception $e){
 		throw $e;
 	}
